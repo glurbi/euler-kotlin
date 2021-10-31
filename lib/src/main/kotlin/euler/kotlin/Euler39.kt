8c45bab069a419444aedc0ed865b93e25a5698d7
Vincent <glurbi@gmail.com>
2021-10-31 16:33:36 +0100

    flow

--- lib/src/main/kotlin/euler/kotlin/Euler39.kt
+++ lib/src/main/kotlin/euler/kotlin/Euler39.kt
@@ -1,36 +1,43 @@
 package euler.kotlin
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import kotlin.system.measureTimeMillis
 
 object Euler39 {
-    fun euler39(): String {
-        var pMax = 0
-        var solMax = 0
-        (1..1000).forEach { p ->
-            print("p=${p} ")
-            var solCount = 0
-            (1..p - 2).forEach { a ->
-                (1..p - a - 1).forEach { b ->
-                    val c = p - a - b
-                    if (a * a == b * b + c * c) {
-                        print("{$a,$b,$c}")
-                        solCount++
-                    }
+
+    private fun perimeters() = (1..1000).asFlow()
+
+    private fun countSolutions(p: Int): Flow<Pair<Int, Int>> = flow {
+        var solCount = 0
+        (1 until p - 2).forEach { a ->
+            (1 until p - a).forEach { b ->
+                val c = p - a - b
+                if (a * a == b * b + c * c) {
+                    solCount++
                 }
             }
-            if (solCount > solMax) {
-                solMax = solCount
-                pMax = p
-            }
-            println()
         }
-        return "$pMax,$solMax"
+        emit(Pair(p, solCount))
+    }
+
+    @FlowPreview
+    suspend fun euler39() = withContext(Dispatchers.Default) {
+        perimeters()
+            .flatMapMerge { p -> countSolutions(p) }
+            .reduce { acc, v -> if (acc.second < v.second) v else acc }
     }
 
 }
 
+@FlowPreview
 fun main() {
     measureTimeMillis {
-        println("answer: ${Euler39.euler39()}")
+        runBlocking {
+            println("answer: ${Euler39.euler39()}")
+        }
     }.also { println("Execution time is ${it} milliseconds") }
 }
